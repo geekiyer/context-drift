6b9076262ea6304fa5900c5d2011a75e70b770a0
geekiyer <gopal87us@gmail.com>
2026-03-25 16:09:41 -0700

    feat: #14 Export new functions in the public API

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,14 @@ export type {
 	Config,
 	FileResult,
 	ScanResult,
+	SemanticCheckRequest,
+	SemanticCheckResponse,
 	StalenessInfo,
 } from "./checkers/types.js";
+export {
+	commitSemanticChecks,
+	prepareSemanticChecks,
+} from "./checkers/semantic.js";
 export { discoverContextFiles, loadConfig } from "./config.js";
 export { parseContextFile } from "./parsers/context-file.js";
 export { reportConsole } from "./reporters/console.js";
